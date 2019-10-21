f2e46f72de4cf899c3e1e627f5b722213a108d10
Jan Frederick Eick <jan-frederick.eick@uni-weimar.de>
2019-10-21 14:17:34 +0200

    remove files

--- hailstone.hs
+++
@@ -1,5 +0,0 @@
-hailstone :: Integer -> Integer
-hailstone n
-        | n `mod` 2 == 0 = n`div`2
-        | otherwise      = n * 2 + 1
-main = print(hailstone 3)
--- sumtorial.hs
+++
@@ -1,5 +0,0 @@
-
-sumtorial :: Integer -> Integer
-sumtorial 0 = 0
-sumtorial n = n + sumtorial(n-1)
-main = print(sumtorial 10)
